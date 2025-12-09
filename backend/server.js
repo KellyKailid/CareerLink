bee8082e324bcdf315c8e7a0b1e5a8f366b2fa32
KailiKelly <daijingyu@KailiKelly-3.local>
2025-12-08 22:22:23 -0800

    small changes

--- backend/server.js
+++ backend/server.js
@@ -47,8 +47,9 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const HOST = '0.0.0.0';
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
 
